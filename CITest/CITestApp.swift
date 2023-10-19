52cfdb1f5cabc1b7ff6eb1f74cf631b9fc871a84
Tomasz Kubicki <tjpk@poczta.onet.pl>
2023-10-19 18:30:48 +0200

    Initial Commit

---
+++ CITest/CITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CITestApp.swift
+//  CITest
+//
+//  Created by Tomasz Kubicki on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
